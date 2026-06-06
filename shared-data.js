1d41faa7ac50fddf4cd0e37e5777c441fc9e1589
finnwilson4 <148872186+finnwilson4@users.noreply.github.com>
2026-06-06 22:33:41 +0100

    Add files via upload

---
+++ shared-data.js
@@ -0,0 +1,150 @@
+const workouts = {
+    arms: {
+        name: "Arms",
+        exercises: [
+            {
+                name: "Bicep Curls",
+                weight: "8kg",
+                muscle: "Bicep"
+            },
+            {
+                name: "Hammer Curls",
+                weight: "8kg",
+                muscle: "Brachialis"
+            },
+            {
+                name: "Forearm Curls",
+                weight: "6kg",
+                muscle: "Brachio Radialis"
+            },
+            {
+                name: "Finger Curls",
+                weight: "6kg",
+                muscle: "Inner Forearm"
+            },
+            {
+                name: "Tricep Curls",
+                weight: "8kg",
+                muscle: "Tricep Longhead"
+            },
+            {
+                name: "Tricep Extensions",
+                weight: "6kg",
+                muscle: "Tricep Shorthead"
+            }
+        ]
+    },
+
+    legs: {
+        name: "Legs",
+        exercises: [
+            {
+                name: "Goblet Squats",
+                weight: "8kg",
+                muscle: "Quads"
+            },
+            {
+                name: "Dumbbell Lowers",
+                weight: "8kg",
+                muscle: "Hamstrings"
+            },
+            {
+                name: "Bulgarian Squats",
+                weight: "8kg",
+                muscle: "Glutes"
+            },
+            {
+                name: "Wide Squats",
+                weight: "8kg",
+                muscle: "Adductors"
+            },
+            {
+                name: "Calf Raises",
+                weight: "8kg",
+                muscle: "Calves"
+            }
+        ]
+    },
+
+    back: {
+        name: "Back",
+        exercises: [
+            {
+                name: "Bent-Over Rows (In)",
+                weight: "8kg",
+                muscle: "Mid Back"
+            },
+            {
+                name: "Knelt-Over Raises",
+                weight: "8kg",
+                muscle: "Lats"
+            },
+            {
+                name: "Shoulder Extensions",
+                weight: "8kg",
+                muscle: "Rear Delts"
+            },
+            {
+                name: "Bent-Over Rows (Out)",
+                weight: "8kg",
+                muscle: "Upper Back"
+            },
+            {
+                name: "Shoulder Shrugs",
+                weight: "8kg",
+                muscle: "Upper Traps"
+            }
+        ]
+    },
+
+    chestShoulders: {
+        name: "Chest/Shoulders",
+        exercises: [
+            {
+                name: "Arm Raises",
+                weight: "8kg",
+                muscle: "Front Delts"
+            },
+            {
+                name: "Lateral Raises",
+                weight: "8kg",
+                muscle: "Lateral Delts"
+            },
+            {
+                name: "Shoulder Extensions",
+                weight: "8kg",
+                muscle: "Rear Delts"
+            },
+            {
+                name: "Downward Press",
+                weight: "8kg",
+                muscle: "Lower Chest"
+            },
+            {
+                name: "Flat Press",
+                weight: "8kg",
+                muscle: "Lower Chest"
+            },
+            {
+                name: "Cross Body Raises",
+                weight: "6kg",
+                muscle: "Upper Chest"
+            },
+            {
+                name: "Chest Flies",
+                weight: "6kg",
+                muscle: "Mid Chest"
+            }
+        ]
+    }
+};
+
+const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+];
---
+++ workout-page.js
@@ -0,0 +1,115 @@
+
+const container = document.getElementById("workout-container");
+const scheduleContainer = document.getElementById("schedule-container");
+
+// Populate Workout Types
+for (const workout of Object.values(workouts)) {
+    let rows = "";
+
+    workout.exercises.forEach(exercise => {
+        rows += `
+            <tr>
+                <td style="width: 50%">${exercise.name}</td>
+                <td style="width: 15%">${exercise.weight}</td>
+                <td style="width: 35%">${exercise.muscle}</td>
+            </tr>
+        `;
+    });
+
+    container.innerHTML += `
+        <div class="card workout">
+            <h2>${workout.name}</h2>
+
+            <table class="workout-description-table">
+                ${rows}
+            </table>
+        </div>
+    `;
+}
+
+// Generate workout options
+let workoutOptions = `
+    <option value="">Rest Day</option>
+`;
+
+for (const [key, workout] of Object.entries(workouts)) {
+    workoutOptions += `
+        <option value="${key}">
+            ${workout.name}
+        </option>
+    `;
+}
+
+// Generate schedule table
+let headers = "";
+let selects = "";
+
+days.forEach(day => {
+
+    headers += `
+        <th>${day}</th>
+    `;
+
+    selects += `
+        <td>
+            <select class="workout-select" data-day="${day.toLowerCase()}">
+                ${workoutOptions}
+            </select>
+        </td>
+    `;
+});
+
+scheduleContainer.innerHTML = `
+    <table class="workout-schedule-table">
+        <tr>${headers}</tr>
+        <tr>${selects}</tr>
+    </table>
+`;
+console.log("Script loaded");
+document.querySelectorAll(".workout-select").forEach(select => {
+    console.log("Select found:", select);
+
+    select.addEventListener("change", () => {
+        console.log("Changed:", select.dataset.day, select.value);
+    });
+});
+// Save Selected Options
+document.querySelectorAll(".workout-select").forEach(select => {
+    select.addEventListener("change", () => {
+        const day = select.dataset.day;
+
+        localStorage.setItem(
+            `workout-${day}`,
+            select.value
+        );
+    });
+});
+document.querySelectorAll(".workout-select").forEach(select => {
+    const day = select.dataset.day;
+    const savedWorkout = localStorage.getItem(
+        `workout-${day}`
+    );
+
+    if (savedWorkout !== null) {
+        select.value = savedWorkout;
+    }
+});
+
+document.querySelectorAll(".workout-select").forEach(select => {
+
+    select.addEventListener("change", () => {
+
+        const schedule =
+            JSON.parse(localStorage.getItem("workoutSchedule")) || {};
+
+        const day = select.dataset.day;
+        schedule[day] = select.value;
+
+        localStorage.setItem(
+            "workoutSchedule",
+            JSON.stringify(schedule)
+        );
+
+    });
+
+});
