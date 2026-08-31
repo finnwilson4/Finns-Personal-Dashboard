23a48a68686d7a92d8ae88e23cd83f5fcff1d820
finnwilson4 <148872186+finnwilson4@users.noreply.github.com>
2026-08-31 09:37:09 +0100

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dpgpkhzlpsbrypfribuc.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_kI5Pt0JK7VEa_XSjPgc-FQ_6vBRP-H9";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
